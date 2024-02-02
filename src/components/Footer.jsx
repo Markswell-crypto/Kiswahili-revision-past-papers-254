534b29ebc193bcd11e2d2dc01ac5a0a31304d743
Markswell <osoromarkswell80@gmail.com>
2024-02-02 14:03:53 +0300

    Add Logo and Style Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,52 @@
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+import { Link } from 'react-router-dom';
+import { animateScroll as scroll } from 'react-scroll';
+import { IoIosArrowUp } from 'react-icons/io';
 
 function Footer() {
+  const handleScrollToTop = () => {
+    scroll.scrollToTop({
+      duration: 1000,
+      smooth: 'easeInOutQuart',
+    });
+  };
+
   return (
-    <div>
-        {/* <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/exam-papers">Exam Papers</a></li>
-          <li><a href="/payment">Payment</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav> */}
-    </div>
-  )
+    <footer className="bg-dark text-light py-3">
+      <div className="container">
+        <div className="row">
+        <div className="col-md-4 text-center">
+            <Image src="/klogo.png" alt="Kiswahili Revision PastPapers" fluid height="100px" width="100px" />
+            <h1 className="logo">Kiswahili Revision PastPapers</h1>
+          </div>
+          <div className="col-md-2 mt-2">
+            <p className='text-center'>Quick Links</p>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/exam-papers">Exam Papers</Link></li>
+              <li><Link to="/payment">Payment</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </ul>
+          </div>
+          <div className="col-md-5 mt-3 text-center">
+            <p>Contact Information</p>
+            <p>Phone: +254722125349</p>
+            <p>Email: ogindamomanyi@gmail.com</p>
+          </div>
+          <div className='col-md-1 mt-5 mr-5'>
+          <div className="scroll-to-top" onClick={handleScrollToTop}>
+        <IoIosArrowUp size={32} />
+        <p className="text-light mb-0">To Top</p>
+      </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-light text-center py-2">
+        &copy; 2024 MAXOTECH. All rights reserved.
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,23 +6,23 @@ const Header = () => {
   return (
     <div className="container mt-3">
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Image src="/gcomlogo.jpg" alt="Kenya Pastpapers Logo" fluid height="50px" width="50px" className="ml-4" />
+        <Image src="/klogo.png" alt="Kenya Pastpapers Logo" fluid height="100px" width="100px" className="ml-4" />
         <Navbar.Brand as={Link} to="/" className="ml-5">
           KISWAHILI REVISION PASTPAPERS
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/" className="ml-4">
+          <Nav className="ml-5">
+            <Nav.Link as={Link} to="/" style={{ marginLeft: '20px' }}>
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/exam-papers" className="ml-4">
+            <Nav.Link as={Link} to="/exam-papers" style={{ marginLeft: '20px' }}>
               Past-Papers
             </Nav.Link>
-            <Nav.Link as={Link} to="/payment" className="ml-4">
+            <Nav.Link as={Link} to="/payment" style={{ marginLeft: '20px' }}>
               Payment
             </Nav.Link>
-            <Nav.Link as={Link} to="/contact" className="ml-4">
+            <Nav.Link as={Link} to="/contact" style={{ marginLeft: '20px' }}>
               Contact
             </Nav.Link>
           </Nav>
